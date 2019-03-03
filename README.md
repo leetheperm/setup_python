e431d6c6d96b4998983e9fc328ab3b162295ef66
Lee <perm@Lees-MacBook-Pro.local>
2019-03-03 21:28:47 +0000

    Third time's a charm

---
+++ README.md
@@ -0,0 +1,2 @@
+This is just a quicker way to set up python 3 with Sublime text and grab all 
+the pip modules that I need for day to day use.
---
+++ allpipobects.sh
@@ -0,0 +1,24 @@
+#all modules from pip
+
+pip3 install selenium
+pip3 install lxml
+pip3 install send2trash
+pip3 install django
+pip3 install pyautogui
+pip3 install bokeh
+pip3 install pandas
+pip3 install numpy
+pip3 install appium
+pip3 install zipfile
+pip3 install bs4
+pip3 install requests
+pip3 install pyperclip
+pip3 install openpyxl
+pip3 install pyPDF2
+pip3 install pyzmail
+pip3 install pyobj-core
+pip3 install pyobj
+pip3 install twilio
+pip3 install imapclient
+pip3 install pillow
+pip3 install mechanical soup
